fa47a2323c7956488572a78e43b323b8fa1231a7
jnsltk <janos.litkei@gmail.com>
2023-01-21 14:26:50 +0100

    Change __init__.py to load config

--- szotar_net/__init__.py
+++ szotar_net/__init__.py
@@ -1,6 +1,36 @@
 import configparser
+import re
+import os
+import sys
 
-path = "etc/szotar_net/config.ini"
-config = configparser.ConfigParser()
-config.read(path)
 
+def read_config(prefix, rel='etc/szotar_net/config.ini'):
+    path = os.path.join(prefix, rel)
+    if os.path.isfile(path):
+        cp = configparser.ConfigParser()
+        cp.read(path)
+        cp.set('paths', 'prefix', prefix)
+        return cp
+    else:
+        return None
+
+
+def find_config():
+    # detect installation prefix. is there a better way of doing this?
+
+    dirname = os.path.dirname(__file__)
+
+    prefixes = []
+    prefixes.append(os.path.realpath(os.path.join(dirname, '..')))
+    if re.search('/lib/', dirname):
+        prefixes.append(re.sub('/lib/.*', '', dirname))
+    prefixes.append(sys.prefix)
+
+    for prefix in prefixes:
+        cp = read_config(prefix)
+        if cp: return cp
+
+    return None
+
+
+config = find_config()
